fdb8bb2a702302b94c2aa78fe8441440cdf8a3cd
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2023-09-11 07:11:06 +0530

    Update Knapsack.cpp

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -1,89 +1,184 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void knapsack(int n, float weight[], float profit[], float capacity)
+struct bag
 {
-    float arr[n];
-    int i;
-    int c = capacity;
-    float totalProfit = 0;
+    double ratio;
+    int price;
+    int weight;
+};
 
-    for ( i = 0; i < n; i++)
-    {
-        arr[i] = 0;
-    }
-
-    for ( i = 0; i < n; ++i)
-    {
-        if (weight[i] > c)
-            break;
-        else
-        {
-            arr[i] = 1;
-            totalProfit = totalProfit + profit[i];
-            c = c - capacity;
-        }
-    }
-
-    if (i < n)
-        arr[i] = c / weight[i];
-
-    totalProfit = totalProfit + (arr[i] * profit[i]);
-
-    cout << "RESULT" << endl;
-    for (i = 0; i < n; ++i)
-    {
-        cout << arr[i] << endl;
-    }
-
-    cout << "Total profit: " << totalProfit << endl;
-
-
+bool compare(bag a, bag b)
+{
+    return (a.ratio>b.ratio);
 }
 
-int main() {
+class knapsack
+{
+private:
+    int n, capacity, result;
+    bag array[500];
+    int val[500], wt[500];
+    bool status = false;
 
-    int n;
-    float weight[n], profit[n], capacity, ratio[n];
-    float temp;
+public:
+    void input();
+    void display();
+    void maxi();
+    void greedy();
+    void dynamic();
+};
 
+void knapsack::input() {
+    int i = 0;
     cout << "Enter no of items: ";
     cin >> n;
 
-    cout << "Enter Weight and profit (w,p): " << endl;
-    for(int i = 0; i < n; i++)
+    while(i < n)
     {
-        cin >> weight[i] >> profit[i];
+        int a,b;
+        cout << "\nItem" << i+1 << ": Value and Weight: ";
+        cin >> a >> b;
+
+        if (a>0 && b>0)
+        {
+            val[i] = a;
+            wt[i] = b;
+            array[i].price = a;
+            array[i].weight = b;
+            array[i].ratio = (double)array[i].price/array[i].weight;
+            i++;
+        }
+
+        else
+            cout << "Enter valid value and weight!" << endl;
     }
 
-    cout << "Enter capacity: " ;
+    cout << "Enter the capacity of bag: ";
     cin >> capacity;
-    
-    for(int i = 0; i < n; i++)
-        ratio[i] = profit[i] / weight[i];
+
+    while(capacity <0 )
+    {
+        cout << "Capacity cant be negative, please enter again!" << endl;
+        cout << "Enter the capacity of bag: ";
+        cin >> capacity;
+    }
+
+    cout << endl;
+    status = true;
+}
+
+void knapsack::display() {
+    if(!status)
+    {
+        cout << "Enter item into bag!" << endl;
+        return;
+    }
+
+    cout << "------------------------------------------\n";
+    cout << "\tValue" << "\tWeight" << endl;
+    for (int i = 0; i < n; i++)
+        cout << "\t" << val[i] << "\t" << wt[i] << endl;
+    cout << "-------------------------------------------\n";
+}
+
+void knapsack::greedy() {
+    if(!status)
+    {
+        cout << "Enter item into bag!" << endl;
+        return;
+    }
+
+    int weight = capacity;
+    sort(array,array+n,compare);
+    //display();
 
     for (int i = 0; i < n; i++)
     {
-        for(int j = i + 1; j < n; j++)
+        if (weight>array[i].weight)
         {
-            if(ratio[i] < ratio[j])
+            result += array[i].price;
+            weight -= array[i].weight;
+            continue;
+        }
+
+        result += array[i].ratio * weight;
+        break;
+    }
+
+    cout << "Greedy algo applied successfully!" << endl;
+}
+
+void knapsack::dynamic() {
+    if(!status)
+    {
+        cout << "Enter item into bag!" << endl;
+        return;
+    }
+    //int c = capacity+1;
+    //int dp[] = {0};
+    int dp[capacity + 1]={0};
+    for (int i = 0; i < n; i++)
+    {
+        for(int w = capacity; w >= 0; w--)
+        {
+            if(wt[i] <= w)
             {
-                temp = ratio[j];
-                ratio[j] = ratio[i];
-                ratio[i] = temp;
-
-                temp = weight[j];
-                weight[j] = weight[i];
-                weight[i] = temp;
-
-                temp = profit[j];
-                profit[j] = profit[i];
-                profit[i] = temp;
+                dp[w] = max(dp[w],dp[w - wt[i]] + val[i]);
             }
         }
     }
 
-    knapsack(n,weight,profit,capacity);
+    result = dp[capacity];
+    cout << "Dynamic approach applied successfully!" << endl;
+}
+
+void knapsack::maxi() {
+    cout << "\nMaximum value in knapsack: " << result << endl;
+}
+
+int main ()
+{
+    int ch;
+    knapsack k;
+
+    cout << "--------- WELCOME ---------" << endl;
+
+    while(1)
+    {
+        cout << "1. Input" << endl;
+        cout << "2. Display" << endl;
+        cout << "3. Greedy" << endl;
+        cout << "4. Dynamic" << endl;
+        cout << "5. Max possible value" << endl;
+        cout << "6. exit" << endl;
+        cout << "enter your choice: ";
+        cin >> ch;
+
+        switch (ch) {
+            case 1:
+                k.input();
+                break;
+            case 2:
+                k.display();
+                break;
+            case 3:
+                k.greedy();
+                break;
+            case 4:
+                k.dynamic();
+                break;
+            case 5:
+                k.maxi();
+                break;
+            case 6:
+                exit(0);
+                break;
+            default:
+                cout << "Enter valid choice!!" << endl;
+                break;
+        }
+    }
 
     return 0;
 }
